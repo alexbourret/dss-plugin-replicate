f916988962accb8dca270b77793658fd526382aa
admin <admin@localhost>
2024-04-11 15:22:40 +0200

    Edited file 'resource/browse_replicate.py'

--- resource/browse_replicate.py
+++ resource/browse_replicate.py
@@ -1,15 +1,14 @@
 import replicate
 from replicate_common import (DSSSelectorChoices, get_api_token)
+from replicate_client import (ReplicateClient)
 
 def do(payload, config, plugin_config, inputs):
     print("ALX:do:payload={}, config={}".format(payload, config))
     # LX:do:payload={'parameterType': 'SELECT', 'parameterName': 'model', 'customChoices': True, 'rootModel': {'auth_type': 'service-api-token', 'parameter2': 42, 'service_token': {'mode': 'PRESET', 'name': 'Alex B'}}}, config={'auth_type': 'service-api-token', 'parameter2': 42, 'service_token': {'api_token': 'r8_ehs227RNtM3VNUQ8RqiuPdWHoCeO5Zc4PGGPF'}}
     api_token = get_api_token(config)
-    print("ALX:{}".format(api_token))
     choices = DSSSelectorChoices()
     parameter_name = payload.get("parameterName")
-    print("ALX:parameter_name={}".format(parameter_name))
     if parameter_name == "model":
-        print("ALX:model!")
+        client = ReplicateClient(api_token=api_token)
         return choices.text_message(api_token)
     return choices.to_dss()
