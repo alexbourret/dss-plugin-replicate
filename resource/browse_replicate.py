7ab3b5fea184ea497b698dc40390965ea2d6fe60
admin <admin@localhost>
2024-04-11 15:08:03 +0200

    Edited files

--- resource/browse_replicate.py
+++ resource/browse_replicate.py
@@ -2,5 +2,6 @@ import replicate
 from replicate_common import (DSSSelectorChoices)
 
 def do(payload, config, plugin_config, inputs):
+    print("ALX:do:payload={}, config={}".format(payload, config))
     choices = DSSSelectorChoices()
     return choices.to_dss()
