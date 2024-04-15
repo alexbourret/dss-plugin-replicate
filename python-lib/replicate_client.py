8984af4a007e1946549b0b0ad72e806cf1751e4f
admin <admin@localhost>
2024-04-15 14:04:02 +0200

    Edited file 'python-lib/replicate_client.py'

--- python-lib/replicate_client.py
+++ python-lib/replicate_client.py
@@ -1,8 +1,11 @@
-import replicate
+import requests
+from replicate_auth import ReplicateAuth
 
-class ReplicateClient(object):
+
+class ReplicateSession(object):
     def __init__(self, api_token=None):
-        self.api_token = api_token
+        self.session = request.Session()
+        self.session.auth = ReplicateAuth(access_token=api_token)
 
     def get_available_models(self):
         collections = [collection for page in replicate.paginate(replicate.collections.list) for collection in page]
